3ed5363ec2a88db645f8722ba45c7545b58787be
AnthonyBenjaminSakala4 <43514038+AnthonyBenjaminSakala4@users.noreply.github.com>
2021-04-28 22:04:25 +0200

    Update my_alu_db_Export.sql

--- my_alu_db_Export.sql
+++ my_alu_db_Export.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 28, 2021 at 10:01 PM
+-- Generation Time: Apr 25, 2021 at 10:01 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.12
 
